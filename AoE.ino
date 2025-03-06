6010115c6ab5164605cb3334e679f96f3fd1a495
aginz77 <109112075+aginz77@users.noreply.github.com>
2025-03-06 17:21:17 -0500

    Adjusted to work continuously for 2+ buttons

--- AoE.ino
+++ AoE.ino
@@ -1,34 +1,96 @@
-#define light1 22
-#define button1 23
-char light;
-unsigned long time1;
+#include <math.h>
+
+#define LIGHT1 22
+#define BUTTON1 23
+
+int numButtons = 2;
+
+int buttons[2];
+int leds[2];
+unsigned long times[2];
+
+unsigned long previousButtonAssignment;
+unsigned long buttonDelay = 4000;
+unsigned long startingTime;
+
+int score = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(light1, OUTPUT);
-  pinMode(button1, INPUT_PULLUP);
-  light = true;
-  time1 = millis();
+  startingTime = millis();
+
+  for (int i = 0; i < numButtons; i++) {
+
+    pinMode(22 + 2 * i, OUTPUT); // initialize LED pins
+    pinMode(23 + 2 * i, INPUT_PULLUP); // initialize button pins
+
+    buttons[i] = 1; // initialize buttons to 1 (unpressed)
+  }
 
   Serial.begin(9600);
+
+  Serial.println("STARTING: ");
+  Serial.println("+-----------------+");
+  for (int i = 0; i < numButtons; i++) {
+    Serial.print("BUTTON i = " + i);
+    Serial.println(digitalRead(23 + 2 * i));
+  }
+  for (int i = 0; i < numButtons; i++) {
+    Serial.print("LED i = " + i);
+    Serial.println(leds[i]);
+  }
+  Serial.println("+-----------------+");
 }
 
 void loop() {
+  unsigned long t = millis(); // current time
+
   // put your main code here, to run repeatedly:
-  if (light) {
-    digitalWrite(light1, HIGH);
-  } else {
-    digitalWrite(light1, LOW);
-  }
+  for (int i = 0; i < numButtons; i++) {
+    if (leds[i] == 1 && times[i] != 0 && t - times[i] >= buttonDelay) { // if light expired
+      times[i] = 0; // Reset LED's time 
+      leds[i] = 0; // turn off LED
+    }
 
-  if (light && millis() - time1 >= 4000) {
-    digitalWrite(light1, LOW);
-    light = false;
-  }
+    digitalWrite(22 + 2 * i, leds[i]); // update LEDs
 
-  if (digitalRead(button1) == LOW) { 
-    light = false;
+    buttons[i] = digitalRead(23 + 2 * i); // update buttons array
+
+    if (buttons[i] == 0) { // if button[i] pressed
+      if (leds[i] == 1) {
+        leds[i] = 0;                        // turn off leds[i]
+        score++;
+      } else {
+        score--;
+      }
+    }
   }
   
-  delay(100);
+  if (t - previousButtonAssignment >= buttonDelay) { // if delay has passed light up on a new button
+    previousButtonAssignment = t;
+    int n = random(0, numButtons); // pick random led
+    while (leds[n] == 1) {        // if already lit
+      n = random(0, numButtons); // pick new button
+    }
+    leds[n] = 1;        // light that button
+    int diff = t - startingTime;
+
+    buttonDelay = 3000 * exp(-8.47e-6 * diff) + 1000; // decrease button delay
+    printStuff(t, n);
+  }
+}
+
+void printStuff(unsigned long t, int n) {
+  Serial.println("+-----------------+");
+  Serial.println(t);
+  Serial.println(n);
+  for (int i = 0; i < numButtons; i++) {
+    Serial.print("BUTTON i = " + i);
+    Serial.println(digitalRead(23 + 2 * i));
+  }
+  for (int i = 0; i < numButtons; i++) {
+    Serial.print("LED i = " + i);
+    Serial.println(leds[i]);
+  }
+  Serial.println("+-----------------+");
 }
